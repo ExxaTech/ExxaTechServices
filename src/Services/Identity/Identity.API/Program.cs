468e75da3d32ad95ac613630a6de17008de2f0e0
Milton Reis <milton.reis@kroton.com.br>
2018-04-29 14:15:47 -0300

    Add Identity

---
+++ src/Services/Identity/Identity.API/Program.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.eShopOnContainers.Services.Identity.API;
+using Microsoft.eShopOnContainers.Services.Identity.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Identity.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args)
+                .MigrateDbContext<PersistedGrantDbContext>((_, __) => { })
+                .MigrateDbContext<ApplicationDbContext>((context, services) =>
+                {
+                    var env = services.GetService<IHostingEnvironment>();
+                    var logger = services.GetService<ILogger<ApplicationDbContextSeed>>();
+                    var settings = services.GetService<IOptions<AppSettings>>();
+
+                    new ApplicationDbContextSeed()
+                        .SeedAsync(context, env, logger, settings)
+                        .Wait();
+                })
+                .Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
